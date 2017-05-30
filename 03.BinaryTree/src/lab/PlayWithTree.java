d1321ce21edf1422a7bfc13c477c1ce23d82fb7d
mitkovaelena <mitkova_elena@yahoo.com>
2017-05-30 17:22:21 +0300

    03.Binary Tree - Lab

---
+++ 03.BinaryTree/src/lab/PlayWithTree.java
@@ -0,0 +1,64 @@
+package lab;
+
+import lab.binaryTree.BinaryTree;
+import lab.tree.Tree;
+
+public class PlayWithTree {
+
+    public static void main(String[] args) {
+        Tree<Integer> tree =
+                new Tree<>(7,
+                        new Tree<>(19,
+                                new Tree<>(1),
+                                new Tree<>(12),
+                                new Tree<>(31)),
+                        new Tree<>(21),
+                        new Tree<>(14,
+                                new Tree<>(23),
+                                new Tree<>(6)));
+
+        System.out.println("lab.tree.Tree (indented):");
+        String output = tree.print(0, new StringBuilder());
+        System.out.println(output);
+
+        System.out.println("lab.tree.Tree (DFS):");
+        Iterable<Integer> returnedList = tree.orderDFS();
+        for(Integer i : returnedList) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        System.out.println("lab.tree.Tree (BFS):");
+        Iterable<Integer> returnedList2 = tree.orderBFS();
+        for(Integer i : returnedList2) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        System.out.println("lab.tree.Tree nodes:");
+        tree.each(e -> System.out.print(" " + e));
+        System.out.println();
+
+        System.out.println();
+        BinaryTree<String> binaryTree =
+                new BinaryTree<>("*",
+                        new BinaryTree<>("+",
+                                new BinaryTree<>("3"),
+                                new BinaryTree<>("2")),
+                        new BinaryTree<>("-",
+                                new BinaryTree<>("9"),
+                                new BinaryTree<>("6")));
+
+        System.out.println("Binary tree (indented, pre-order):");
+        output = binaryTree.printIndentedPreOrder(0, new StringBuilder());
+        System.out.println(output);
+
+        System.out.println("Binary tree nodes (in-order):");
+        binaryTree.eachInOrder(e -> System.out.print(" " + e));
+        System.out.println();
+
+        System.out.println("Binary tree nodes (post-order):");
+        binaryTree.eachPostOrder(e -> System.out.print(" " + e));
+        System.out.println();
+    }
+}
---
+++ 03.BinaryTree/src/lab/tree/Tree.java
@@ -0,0 +1,79 @@
+package lab.tree;
+
+import java.util.*;
+import java.util.function.Consumer;
+
+public class Tree<T> {
+    private T value;
+    private List<Tree<T>> children;
+
+    public Tree(T value, Tree<T>... children) {
+        this.value = value;
+        this.children = new ArrayList<>();
+        Collections.addAll(this.children, children);
+    }
+
+    public Tree(T value) {
+        this.value = value;
+        this.children = new ArrayList<>();
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public List<Tree<T>> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Tree<T>> children) {
+        this.children = children;
+    }
+
+    // append output to builder
+    public String print(int indent, StringBuilder builder) {
+        builder.append(new String(new char[indent*2]).replace("\0", " ")).append(this.value + "\n");
+        for(Tree child : this.children){
+            child.print(indent+1, builder);
+        }
+        return builder.toString();
+    }
+
+    public void each(Consumer<T> consumer) {
+        consumer.accept(this.getValue());
+        for(Tree<T> child : this.children){
+            child.each(consumer);
+        }
+    }
+
+    public Iterable<T> orderDFS() {
+        List<T> dfsOrderedNodes = new ArrayList<T>();
+        this.DFS(this, dfsOrderedNodes);
+        return dfsOrderedNodes;
+    }
+
+    private void DFS(Tree<T> parentNode, List<T> dfsOrderedNodes) {
+        for(Tree<T> child : parentNode.getChildren()){
+            DFS(child, dfsOrderedNodes);
+        }
+        dfsOrderedNodes.add(parentNode.getValue());
+    }
+
+    public Iterable<T> orderBFS() {
+        List<T> bfsOrderedNodes = new ArrayList<T>();
+        Deque<Tree<T>> queue = new ArrayDeque<Tree<T>>();
+        queue.add(this);
+        while (!queue.isEmpty()){
+            Tree<T> node = queue.poll();
+            for(Tree<T> child : node.getChildren()){
+                queue.add(child);
+            }
+            bfsOrderedNodes.add(node.getValue());
+        }
+        return bfsOrderedNodes;
+    }
+}
