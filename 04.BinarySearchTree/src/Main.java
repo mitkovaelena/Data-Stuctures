da7deb62940bafb59ddd4d98e01b29ad6c96c8f1
mitkovaelena <mitkova_elena@yahoo.com>
2017-06-08 01:24:28 +0300

    unnecessary files deleted

--- 04.BinarySearchTree/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
-        System.out.println(bst.getRoot());
-        bst.insert(6);
-        System.out.println(bst.contains(6));
-    }
-}
