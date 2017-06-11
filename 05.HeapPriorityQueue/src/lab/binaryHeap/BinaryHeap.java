f374061287ee16f1c974c36367ca3fca12fa63da
mitkovaelena <mitkova_elena@yahoo.com>
2017-06-11 17:05:55 +0300

    05.HeapPriorityQueue - Lab

---
+++ 05.HeapPriorityQueue/src/lab/binaryHeap/BinaryHeap.java
@@ -0,0 +1,71 @@
+package lab.binaryHeap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryHeap<T extends Comparable<T>> {
+
+    private List<T> heap;
+    private int size;
+
+    public BinaryHeap() {
+        this.heap = new ArrayList<T>();
+        this.size = 0;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public void insert(T element) {
+        this.heap.add(element);
+        heapifyUp(this.size());
+        size++;
+    }
+
+    private void heapifyUp(int elementInd) {
+        int parentInd = (elementInd - 1) / 2;
+        while (elementInd > 0 && this.heap.get(elementInd).compareTo(this.heap.get(parentInd)) > 0) {
+            T temp = this.heap.get(parentInd);
+            this.heap.set(parentInd, this.heap.get(elementInd));
+            this.heap.set(elementInd, temp);
+            elementInd = parentInd;
+            parentInd = (elementInd - 1) / 2;
+        }
+    }
+
+    public T peek() {
+        return this.heap.get(0);
+    }
+
+    public T pull() {
+        if (this.heap.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        T maxElem = this.heap.get(0);
+        this.size--;
+        this.heap.set(0, this.heap.get(this.size()));
+        this.heap.set(this.size(), maxElem);
+        this.heap.remove(this.size());
+        heapifyDown(0);
+        return maxElem;
+    }
+
+    private void heapifyDown(int elementInd) {
+        while (elementInd < this.size() / 2) {
+            int childInd = 2 * elementInd + 1;
+            if (this.heap.size() > childInd + 1 && this.heap.get(childInd + 1).compareTo(this.heap.get(childInd)) > 0) {
+                childInd += 1;
+            }
+
+            if (this.heap.get(elementInd).compareTo(this.heap.get(childInd)) > 0) {
+                break;
+            }
+
+            T temp = this.heap.get(childInd);
+            this.heap.set(childInd, this.heap.get(elementInd));
+            this.heap.set(elementInd, temp);
+            elementInd = childInd;
+        }
+    }
+}
---
+++ 05.HeapPriorityQueue/src/lab/heapSort/Heap.java
@@ -0,0 +1,37 @@
+package lab.heapSort;
+
+public class Heap {
+
+    public static <E extends Comparable<E>> void sort(E[] array) {
+
+        int n = array.length;
+        for (int i = n / 2; i >= 0; i--) {  //constructHeap
+            heapifyDown(array, i, n);
+        }
+
+        for (int j = n - 1; j > 0; j--) {
+            E temp = array[0];
+            array[0] = array[j];
+            array[j] = temp;
+            heapifyDown(array, 0, j);
+        }
+    }
+
+    private static <E extends Comparable<E>> void heapifyDown(E[] heap, int elementInd, int len) {
+        while (elementInd < len / 2) {
+            int childInd = 2 * elementInd + 1;
+            if (len > childInd + 1 && heap[childInd + 1].compareTo(heap[childInd]) > 0) {
+                childInd += 1;
+            }
+
+            if (heap[elementInd].compareTo(heap[childInd]) > 0) {
+                break;
+            }
+
+            E temp = heap[childInd];
+            heap[childInd] = heap[elementInd];
+            heap[elementInd] = temp;
+            elementInd = childInd;
+        }
+    }
+}
